eb40d2e73bbc1c97c1e438928203c9347f5a268d
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-03-15 00:23:15 -0700

    Adding images to public

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+            <img src="/images/chad-montano-eeqbbemH9-c-unsplash.jpg" alt="" />
+              </div>
               <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
