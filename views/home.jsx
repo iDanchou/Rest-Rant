904fc5bf4381b8426feb83cdb9feb3813f47fee7
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-03-18 23:55:28 -0700

    Navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -10,10 +10,6 @@ function home () {
             <img src="/images/chad-montano-eeqbbemH9-c-unsplash.jpg" alt="A delicious stack of panackes" />
             Photo by <a href="https://unsplash.com/@briewilly">Chad Montano</a> on <a href="https://unsplash.com/">Unsplash</a>
               </div>
-              <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-
           </main>
       </Def>
     )
