71263161df18520dad5cfa82dcd0fc2b2e526e58
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-03-19 20:55:17 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -3,9 +3,14 @@ const places = require('../models/places.js')
 
 // GET /places
 
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
+
 
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -16,10 +21,22 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
 
 
 
@@ -37,6 +54,51 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+
+    // Save the new data into places[id]
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
+  }
+})
+
+
 
 
 module.exports = router
